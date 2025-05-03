16aaeb204a5cd08a07cea02888a54fb3f4652eb0
skroflin <sven.kroflin@gmail.com>
2025-05-03 17:36:21 +0200

    initial commit

---
+++ src/types/types.ts
@@ -0,0 +1,29 @@
+export interface WeatherData {
+    temperature: number;
+    sky: string;
+    is_rainy: boolean;
+    time: string;
+    date: string;
+}
+
+export interface HolidayDate {
+    date: string;
+    name: string;
+}
+
+export interface ProcessedStats {
+    maxTemp: number;
+    avgTemp: number;
+    minTemp: number;
+    skyCountsText: string;
+    rainShowersText: string;
+    skyHolidaysText: string;
+}
+
+export interface EmailConfig {
+    host: string;
+    port: number;
+    user: string;
+    pass: string;
+    to: string;
+}
